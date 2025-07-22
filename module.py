cf04497c2d686ce58236b48991e594593cb29ee9
Rupesh Pathak <rupeshpathak@Rupeshs-MacBook-Air.local>
2025-07-22 19:28:38 +0530

    starting python

---
+++ module.py
@@ -0,0 +1,13 @@
+import pyjokes
+
+#print("printing jokes...")
+jokes = pyjokes.get_jokes()
+print(jokes);
+
+# different kind of comment statment
+
+"""""
+3 Quotes is for mutlple line comment.
+So, let me write in multiple lines of code.
+For commenting single line in mutliple line we can use "cmd + /"
+"""""
